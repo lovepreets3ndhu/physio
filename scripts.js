49ecbc3297b8dc11db16c7e04893391747c8a233
LOVEPREET SINGH <164376195+lovepreets3ndhu@users.noreply.github.com>
2024-10-21 17:37:23 +0000

    changes

--- scripts.js
+++ scripts.js
@@ -35,3 +35,13 @@ function validateForm() {
     }
     return true;
 }
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
