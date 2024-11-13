17cd1ae466a96732fdb591507f9ba8242aa2eb6e
Kinshuk Bhatia <165277821+KinshukBhatia29@users.noreply.github.com>
2024-11-13 13:23:42 +0000

    whyus

--- scripts.js
+++ scripts.js
@@ -68,3 +68,9 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       });
   });
 });
+$(document).ready(function(){
+  $('.treatmentsinr').hover(
+      function() { $(this).find('img').css('transform', 'scale(1.1)'); },
+      function() { $(this).find('img').css('transform', 'scale(1)'); }
+  );
+});
